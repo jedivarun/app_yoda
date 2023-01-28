da8c48e79d671b9b4673b029428cd62059d04271
Jedivarun <varundinesh@gmail.com>
2023-01-28 00:19:35 -0500

    chng

--- yoda_exe.py
+++ yoda_exe.py
@@ -4,6 +4,9 @@ import requests
 # Define the API endpoint
 endpoint = "https://api.funtranslations.com/translate/yoda.json"
 
+# Add the Yoda image
+yoda_img = "https://vignette.wikia.nocookie.net/starwars/images/6/6f/Yoda_SWSB.png/revision/latest/top-crop/width/360/height/450?cb=20180906044831"
+
 def yoda_translator(text):
     # Make a GET request to the endpoint with the text to translate
     response = requests.get(endpoint, params={"text": text})
@@ -14,6 +17,9 @@ def yoda_translator(text):
 
 st.title("Yoda Translator App")
 
+# Show the Yoda image
+st.image(yoda_img, width=200)
+
 # Get text input from the user
 text = st.text_input("Enter text to translate:")
 
@@ -21,3 +27,4 @@ text = st.text_input("Enter text to translate:")
 if st.button("Translate"):
     result = yoda_translator(text)
     st.success(result)
+
